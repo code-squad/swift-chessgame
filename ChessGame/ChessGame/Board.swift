f3bf26669369fd7e2a8ec5e3c3e4b98c2e86c08b
Changhee-Byun <changhee.byun@kinemaster.com>
2019-10-04 13:01:57 +0900

    add missing source files. rename unitest class

--- ChessGame/ChessGame/Board.swift
+++ ChessGame/ChessGame/Board.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+enum ChessGameError : Error {
+    case invalidRange
+}
+
+
+class Board {
+    var pawns : [Pawn]
+    
+    init() {
+        self.pawns = []
+    }
+    
+    func add(pawn: Pawn) {
+        self.pawns.append(pawn)
+    }
+    
+    func pawnCount() -> Int {
+        return pawns.count
+    }
+    
+    func color(pawnIndex: Int) throws -> PawnColor {
+        guard pawnIndex < pawns.count else {
+            throw ChessGameError.invalidRange
+        }
+        
+        return self.pawns[pawnIndex].color
+    }
+}
