9c572d002dbc1447fd56ba1594b8a937ffd18105
hoemoon <hoemoon.song@gmail.com>
2019-10-06 20:04:25 +0900

    step 1

--- ChessGame/ChessGame/main.swift
+++ ChessGame/ChessGame/main.swift
@@ -10,3 +10,38 @@ import Foundation
 
 print("Hello, World!")
 
+struct Pawn {
+	let color: Color
+	
+	enum Color: String {
+		case white
+		case black
+	}
+	
+	init(color: Color) {
+		self.color = color
+	}
+	
+	init?(string: String) {
+		let lowercased = string.lowercased()
+		switch Color(rawValue: lowercased) {
+		case .some(let color):
+			self.color = color
+			return
+		case .none:
+			return nil
+		}
+	}
+}
+
+class Board {
+	private var pawns = [Pawn]()
+	
+	var pawnCount: Int {
+		return pawns.count
+	}
+	
+	func add(_ pawn: Pawn) {
+		pawns.append(pawn)
+	}
+}
