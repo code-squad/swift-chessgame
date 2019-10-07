bd725ba92690678056fc587429d106cac2318120
Changhee-Byun <changhee.byun@kinemaster.com>
2019-10-08 02:54:55 +0900

    implement 2-1, add unit test

--- ChessGame/ChessGame/main.swift
+++ ChessGame/ChessGame/main.swift
@@ -9,4 +9,26 @@
 import Foundation
 
 
+func main() {
+    while true {
+        print("보드게임 명령어를 입력하세요 (s: 시작, e: 끝)")
+        print("> ", terminator: "")
+        let read = readLine() ?? ""
+        
+        switch read {
+        case "s", "S":
+            let board = Board()
+            board.initialize()
+            print(board.print())
+            
+        case "e", "E":
+            break
+            
+        default :
+            print("잘못된 입력입니다.")
+            continue
+        }
+    }
+}
 
+main()
